5bc7b2a186c6ebf1a44726a89cc04f874a5c51b7
Ankush7491 <ankushsharma7491@live.com>
2024-11-26 08:10:20 +0530

    new hibernate Project

---
+++ hibernate/src/main/java/ankush/spring/hibernate/HibernateApp.java
@@ -0,0 +1,13 @@
+package ankush.spring.hibernate;
+
+/**
+ * Hello world!
+ *
+ */
+public class HibernateApp 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
